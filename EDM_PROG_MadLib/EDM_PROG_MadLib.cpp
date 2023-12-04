f8e7e887dee7de9d64d0a61c63afab3cfe94aa96
eloydiazmunoz <eloydiazlsgi@gmail.com>
2023-12-04 12:18:21 +0100

    04/12/2023/12:18

--- EDM_PROG_MadLib/EDM_PROG_MadLib.cpp
+++ EDM_PROG_MadLib/EDM_PROG_MadLib.cpp
@@ -8,23 +8,23 @@ string demanarText(string prompt);
 int demanarNumero(string prompt);
 void explicarHistoria(string nom, string nomPlural, int numero, string partCos, string verb);
 
-int main()
-{
-    cout << "Benvingut a Mad Libs. \n\n";
-    cout << "Respon les seguents preguntes per ajudar a crear una nova historia.\n";
+    int main()
+    {
+        cout << "Benvingut a Mad Libs. \n\n";
+        cout << "Respon les seguents preguntes per ajudar a crear una nova historia.\n";
 
-    // Obtindre les entrades de l'usuari
-    string nom = demanarText("Per favor, introdueix un nom: ");
-    string nomPlural = demanarText("Per favor, introdueix un nom plural: ");
-    int numero = demanarNumero("Per favor, introdueix un numero: ");
-    string partCos = demanarText("Per favor, introdueix una part del cos: ");
-    string verb = demanarText("Per favor, introdueix un verb: ");
+        // Obtindre les entrades de l'usuari
+        string nom = demanarText("Per favor, introdueix un nom: ");
+        string nomPlural = demanarText("Per favor, introdueix un nom plural: ");
+        int numero = demanarNumero("Per favor, introdueix un numero: ");
+        string partCos = demanarText("Per favor, introdueix una part del cos: ");
+        string verb = demanarText("Per favor, introdueix un verb: ");
 
-    // Mostrar la història de Mad Libs
-    explicarHistoria(nom, nomPlural, numero, partCos, verb);
+        // Mostrar la història de Mad Libs
+        explicarHistoria(nom, nomPlural, numero, partCos, verb);
 
-    return 0;
-}
+        return 0;
+    }
 
 /// <summary>
 /// Sol·licita a l'usuari una entrada de text i retorna el text introduït.
@@ -62,10 +62,10 @@ int demanarNumero(string prompt)
 /// <param name="verb">Verb utilitzat a la història.</param>
 void explicarHistoria(string nom, string nomPlural, int numero, string partCos, string verb)
 {
-    cout << "\nAquesta és la teva història de Mad Libs:\n";
+    cout << "\nAquesta es la teva historia de Mad Libs:\n";
     cout << "En una terra llunyana, " << nom << " va decidir visitar el bosc encantat.\n";
-    cout << "Allà, van descobrir un grup de " << nomPlural << " simpàtics.\n";
-    cout << "Sorprenentment, els " << nomPlural << " podien parlar i van demanar " << numero << " desitjos màgics.\n";
+    cout << "Alla, van descobrir un grup de " << nomPlural << " simpatics.\n";
+    cout << "Sorprenentment, els " << nomPlural << " podien parlar i van demanar " << numero << " desitjos magics.\n";
     cout << "Emocionat, " << nom << " va utilitzar la seva " << partCos << " per realitzar una dansa especial.\n";
-    cout << "Al final, el bosc es va convertir en una meravellosa terra de " << verb << ", i tothom va viure feliçment per sempre.\n";
+    cout << "Al final, el bosc es va convertir en una meravellosa terra de " << verb << ", i tothom va viure felicment per sempre.\n";
 }
